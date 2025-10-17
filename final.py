ae43b73dface1d50df7f836c1c002acde4fe1244
manishkuma123 <manishp.dotpitchtechnologies@gmail.com>
2025-10-17 12:03:44 +0530

    data added

--- final.py
+++ final.py
@@ -403,4 +403,4 @@ async def cleanup_job(job_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
